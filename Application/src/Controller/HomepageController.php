2018d77a8abf28598af466f04018a964a1fc0f5f
MZigomar <vincent_robart@hotmail.fr>
2022-03-09 16:10:03 +0100

    tet

--- Application/src/Controller/HomepageController.php
+++ Application/src/Controller/HomepageController.php
@@ -9,10 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
     #[Route('/', name: 'app_homepage')]
 =======
     #[Route('/homepage', name: 'app_homepage')]
 >>>>>>> 69303dd (création de la branche homepage)
+=======
+    #[Route('/', name: 'app_homepage')]
+>>>>>>> 739a3cb (tet)
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
