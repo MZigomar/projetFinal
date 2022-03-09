739a3cb082e82ba5b223d8172d3bef639c4c94ec
MZigomar <vincent_robart@hotmail.fr>
2022-03-09 16:10:03 +0100

    tet

--- Application/src/Controller/HomepageController.php
+++ Application/src/Controller/HomepageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
+    #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
