ea929974e3f8eef7f3210627da8d5dd8b419acc4
MZigomar <vincent_robart@hotmail.fr>
2022-03-11 11:28:34 +0100

    conflit encore

--- Application/src/Controller/HomepageController.php
+++ Application/src/Controller/HomepageController.php
@@ -8,7 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
+<<<<<<< HEAD
     #[Route('/', name: 'app_homepage')]
+=======
+    #[Route('/homepage', name: 'app_homepage')]
+>>>>>>> 69303dd (création de la branche homepage)
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
