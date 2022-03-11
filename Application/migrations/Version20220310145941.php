4aeb787b7ea747e4a0bbbb85eb33b98e756e3b89
dylan herbin <herbin.dylan59@gmail.com>
2022-03-11 12:50:11 +0100

    ajout de la fonctionnalité de création de compte

--- Application/migrations/Version20220310145941.php
+++ Application/migrations/Version20220310145941.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220310135538 extends AbstractMigration
+final class Version20220310145941 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,24 +20,26 @@ final class Version20220310135538 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE cart DROP FOREIGN KEY FK_BA388B725EE16A8');
         $this->addSql('DROP INDEX IDX_BA388B725EE16A8 ON cart');
         $this->addSql('ALTER TABLE cart DROP cart_product_id');
+        $this->addSql('ALTER TABLE cart ADD CONSTRAINT FK_BA388B7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE cart_product ADD cart_id INT NOT NULL, ADD product_id INT NOT NULL');
         $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA1AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id)');
         $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
         $this->addSql('CREATE INDEX IDX_2890CCAA1AD5CDBF ON cart_product (cart_id)');
         $this->addSql('CREATE INDEX IDX_2890CCAA4584665A ON cart_product (product_id)');
-        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD25EE16A8');
         $this->addSql('DROP INDEX IDX_D34A04AD25EE16A8 ON product');
         $this->addSql('ALTER TABLE product DROP cart_product_id, CHANGE reference reference VARCHAR(50) DEFAULT NULL');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849551AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id)');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL, CHANGE roles roles INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart DROP FOREIGN KEY FK_BA388B7A76ED395');
         $this->addSql('ALTER TABLE cart ADD cart_product_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE cart ADD CONSTRAINT FK_BA388B725EE16A8 FOREIGN KEY (cart_product_id) REFERENCES cart_product (id)');
         $this->addSql('CREATE INDEX IDX_BA388B725EE16A8 ON cart (cart_product_id)');
         $this->addSql('ALTER TABLE cart_product DROP FOREIGN KEY FK_2890CCAA1AD5CDBF');
         $this->addSql('ALTER TABLE cart_product DROP FOREIGN KEY FK_2890CCAA4584665A');
@@ -45,8 +47,9 @@ final class Version20220310135538 extends AbstractMigration
         $this->addSql('DROP INDEX IDX_2890CCAA4584665A ON cart_product');
         $this->addSql('ALTER TABLE cart_product DROP cart_id, DROP product_id');
         $this->addSql('ALTER TABLE product ADD cart_product_id INT DEFAULT NULL, CHANGE name name VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE reference reference VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD25EE16A8 FOREIGN KEY (cart_product_id) REFERENCES cart_product (id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD25EE16A8 ON product (cart_product_id)');
-        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE lastname lastname VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE firstname firstname VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849551AD5CDBF');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(100) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE roles roles JSON NOT NULL, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE lastname lastname VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE firstname firstname VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
