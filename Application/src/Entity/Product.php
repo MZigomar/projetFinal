29a7ea74fe9eef417ec08def169eecf18fcf4c33
vvo-senpai <vvopaiement@gmail.com>
2022-03-11 11:16:11 +0100

    productmodif

--- Application/src/Entity/Product.php
+++ Application/src/Entity/Product.php
@@ -19,6 +19,15 @@ class Product
     #[ORM\Column(type: 'string', length: 50, nullable: true)]
     private $reference;
 
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $image;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $stock;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +53,42 @@ class Product
     public function setReference(?string $reference): self
     {
         $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): self
+    {
+        $this->stock = $stock;
 
         return $this;
     }
