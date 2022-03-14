3b187733881ac0573eb701c3d47fe0952dd9811c
dylan herbin <herbin.dylan59@gmail.com>
2022-03-14 13:27:37 +0100

    test de la fonctionnalité du login

--- Application/src/Entity/User.php
+++ Application/src/Entity/User.php
@@ -6,9 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+// @UniqueEntity (fields={"email"}, message="Il y a déjà un compte utilisant cet email")
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
