3f4c0ca240eb733a2c460a120f2d7fe7cc57aba3
dylan herbin <herbin.dylan59@gmail.com>
2022-03-14 17:14:19 +0100

    modification du role pour pouvoir s'incrire

--- Application/src/Entity/User.php
+++ Application/src/Entity/User.php
@@ -20,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $email;
 
-    #[ORM\Column(type: 'integer', nullable:true)]
+    #[ORM\Column(type: 'integer')]
     private $roles;
 
     #[ORM\Column(type: 'string')]
@@ -82,7 +82,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return array_unique($roles);
     }
 
-    public function setRoles(array $roles): self
+    public function setRoles(int $roles): self
     {
         $this->roles = $roles;
 
