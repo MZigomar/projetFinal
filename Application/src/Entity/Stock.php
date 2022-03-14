6c0cd69575c64e2205f662df8dfb785f6a0f11f5
vvo-senpai <vvopaiement@gmail.com>
2022-03-14 15:32:01 +0100

    headerOne

---
+++ Application/src/Entity/Stock.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StockRepository::class)]
+class Stock
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
