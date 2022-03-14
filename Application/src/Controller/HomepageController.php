f16026d92999e215aa4461cc097231a6e899f82c
MZigomar <vincent_robart@hotmail.fr>
2022-03-14 13:08:42 +0100

    liste de produits, après je change de branche, promis

--- Application/src/Controller/HomepageController.php
+++ Application/src/Controller/HomepageController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'acceuil_')]
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'controller_name' => 'la page d\'acceuil',
         ]);
     }
 }
---
+++ Application/src/Controller/ProductsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/produits', name: 'products_')]
+class ProductsController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig');
+    }
+    #[Route('/slug', name: 'details')]
+    public function details(): Response
+    {
+        return $this->render('products/details.html.twig');
+    }
+
+}
