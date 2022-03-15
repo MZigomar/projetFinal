2e2e710b3365135dd3bfd8a72b5d1188f11905a8
dylan herbin <herbin.dylan59@gmail.com>
2022-03-15 15:17:39 +0100

    ajout de la fonctionnalité de modification de mot de passe et installation du bundle pour le mot de passe oublié

--- Application/src/Controller/UsersController.php
+++ Application/src/Controller/UsersController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\EditProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UsersController extends AbstractController
+
+
+class UsersController extends AbstractController 
 {
     #[Route('/users', name: 'users')]
     public function index(): Response
@@ -36,5 +40,30 @@ class UsersController extends AbstractController
         return $this->render('users/editProfile.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+
+    #[Route('/users/pass/modifier', name: 'users_pass_modifier')]
+    public function editPass(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $manager)
+    {
+        if($request->isMethod('POST')){
+            $user = $this->getUser();
+            
+            
+
+            //On vérifie si les 2 mots de passe sont identiques 
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($hasher->hashPassword($user, $request->request->get('pass')));
+                $manager->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+                return $this->redirectToRoute('users');
+
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+
+        }
+        return $this->render('users/editPass.html.twig');
     }
 }
--- Application/src/Entity/User.php
+++ Application/src/Entity/User.php
@@ -6,7 +6,6 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
