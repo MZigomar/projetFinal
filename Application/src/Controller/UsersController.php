dc3ad60a4aaf7bad9ab344d03826494ada2b65da
OLIVIER-Nathan <nathanolivier2811@gmail.com>
2022-03-16 14:55:41 +0100

    supc

--- Application/src/Controller/UsersController.php
+++ Application/src/Controller/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class UsersController extends AbstractController
 {
