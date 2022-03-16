15dcde03896822fc43f9b8299f02d971e28e7be8
OLIVIER-Nathan <nathanolivier2811@gmail.com>
2022-03-16 10:27:15 +0100

    supc

--- Application/src/Controller/UsersController.php
+++ Application/src/Controller/UsersController.php
@@ -8,6 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
+
+    #[Route('/user/{id}/delete', name='user_delete')]
+    #[param User $User]
+    #[return RedirectReponse]
+    public function delete(User $User){
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($User);
+        $em->flush();
+      
+        return $this->redirectToRoute( route: "register");
+  
+  
+  
+  }
+
     #[Route('/users', name: 'users')]
     public function index(): Response
     {
@@ -15,20 +30,6 @@ class UsersController extends AbstractController
             'controller_name' => 'UsersController',
         ]);
     }
-}
 
-
-
- #[Route("/user/{id}/delete", name="user_delete")]
- #[param User $User]
- #[return RedirectReponse]
-
-
-public function delete(User $User)
-{
-    $em = $this->getDoctrine()->getManager();
-    $em->remove($User):
-    $em->flush();
-  
-    return $this->redirectToRoute( route: "register");
+    
 }
