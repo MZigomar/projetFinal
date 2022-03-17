7d554da36f74deac8eff2aa6bcb1ae164c8a4ec7
OLIVIER-Nathan <nathanolivier2811@gmail.com>
2022-03-17 16:21:54 +0100

    test

--- Application/src/Controller/SecurityController.php
+++ Application/src/Controller/SecurityController.php
@@ -29,4 +29,8 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * sos
+     */
 }
