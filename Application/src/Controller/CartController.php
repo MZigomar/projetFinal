5972509246b35182bed549bcba9d0f5179ec42ff
OLIVIER-Nathan <nathanolivier2811@gmail.com>
2022-03-17 17:06:44 +0100

    nofinish

--- Application/src/Controller/CartController.php
+++ Application/src/Controller/CartController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
-    /**
-     * @Route("/panier", name="cart_index")
-     */
+    
+    #[Route("/panier", name:"cart_index")]
+    
     public function index()
     {
         return $this->render('cart/index.html.twig', []);
+    }
+
+    #[Route("/panier/add/{id}", name:"cart_add")]
+
+    public function add($id, Request $request) {
+        $session = $request->getSession();
+
+        $panier = $session->get('panier', []);
+
+        $panier[$id] = 1;
+
+        $session->set('panier', $panier);
+
+        dd($session->get('panier'));
     }
 }
