9dfec0d4b63c87d450d127ba16e3e6e645c6266e
dylan herbin <herbin.dylan59@gmail.com>
2022-03-17 18:55:17 +0100

    manque quelques détails pour le CSS

--- Application/src/Form/EditProfileType.php
+++ Application/src/Form/EditProfileType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EditProfileType extends AbstractType
@@ -13,9 +14,11 @@ class EditProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('Valider', SubmitType::class)
+            ->add('lastname', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('Valider', SubmitType::class, [
+                'attr' => ['class' => 'btn-inscription']
+            ])
         ;
     }
 
--- Application/src/Form/ResetPasswordRequestFormType.php
+++ Application/src/Form/ResetPasswordRequestFormType.php
@@ -17,7 +17,7 @@ class ResetPasswordRequestFormType extends AbstractType
                 'attr' => ['autocomplete' => 'email'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter your email',
+                        'message' => 'Veuillez entrer votre email',
                     ]),
                 ],
             ])
