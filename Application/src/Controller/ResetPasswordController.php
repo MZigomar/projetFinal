ae2da7374e40c9e277cf30b4feec8402ac0128f6
dylan herbin <herbin.dylan59@gmail.com>
2022-03-21 22:54:46 +0100

    Enfin fini 3

--- Application/src/Controller/ResetPasswordController.php
+++ Application/src/Controller/ResetPasswordController.php
@@ -124,7 +124,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('profil_');
+            return $this->redirectToRoute('acceuil_index');
         }
 
         return $this->render('reset_password/reset.html.twig', [
