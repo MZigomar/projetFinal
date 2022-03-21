e932d14e7157badfbe136473cb2f9168be0ec694
vvo-senpai <vvopaiement@gmail.com>
2022-03-21 11:10:38 +0100

    ne pas ajouter

---
+++ Application/src/Controller/SearchController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(): Response
+    
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+    public function searchBar()
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('handleSearch'))
+            ->add('query', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez un mot-clé'
+                ]
+            ])
+            ->add('recherche', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+            ->getForm();
+        return $this->render('search/searchBar.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+        /**
+     * @Route("/handleSearch", name="handleSearch")
+     * @param Request $request
+     */
+    public function handleSearch(Request $request, ProductRepository $repo)
+    {
+        $query = $request->request->get('form')['query'];
+        if($query) {
+            $articles = $repo->findArticlesByName($query);
+        }
+        return $this->render('search/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+}
